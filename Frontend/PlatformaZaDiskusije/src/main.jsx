7371e1b777853e1df28a4ca63852e365701be6ac
perovicc <vukasinperovic10@gmail.com>
2024-12-04 22:39:28 +0100

    bug fix
    bug fix

--- Frontend/PlatformaZaDiskusije/src/main.jsx
+++ Frontend/PlatformaZaDiskusije/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import LoginPage from './Pages/Login.jsx'
-import AdminPage from './pages/AdministratorPage.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
