ff06025842487bfad8af4845bff913e735d2c07e
perovicc <vukasinperovic10@gmail.com>
2024-12-04 22:37:29 +0100

    Starting look of the app
    did the login part and admin page

--- Frontend/PlatformaZaDiskusije/src/main.jsx
+++ Frontend/PlatformaZaDiskusije/src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import LoginPage from './Pages/Login.jsx'
+import AdminPage from './pages/AdministratorPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
