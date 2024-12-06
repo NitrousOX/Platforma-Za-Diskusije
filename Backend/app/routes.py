72b4dd609ebee2ca278b64368664d708a1fe8d7b
David <47796111+NitrousOX@users.noreply.github.com>
2024-12-06 18:38:16 +0100

    Dodata baza u docker

--- Backend/app/routes.py
+++ Backend/app/routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from pymongo import MongoClient
 
 # Create a blueprint for your API
 api = Blueprint('api', __name__)
@@ -6,4 +7,16 @@ api = Blueprint('api', __name__)
 # Define a route for the root URL ("/")
 @api.route('/')
 def hello_world():
+    # Define the MongoDB URI
+    mongodb_uri = "mongodb://root:password@mongodb:27017"
+
+    # Establish a connection to MongoDB
+    client = MongoClient(mongodb_uri)
+
+    # Access the database
+    db = client.get_database('mydb')  # Replace 'mydb' with your database name
     return "Hello, World!"
+
+@api.route('/api/data', methods=['GET'])
+def get_data():
+    return jsonify({'message': 'Hello, world!', 'status': 'success'})
